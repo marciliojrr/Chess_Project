289bd34cb3eaf478e3c5905aed5607e67214f31e
Marcílio Júnior <marciliojr@outlook.com>
2022-03-22 20:10:29 -0300

    Criando a classe posicao

---
+++ Xadrez-Console/Tabuleiro/Position.cs
@@ -0,0 +1,22 @@
+﻿namespace Tabuleiro
+{
+    internal class Position
+    {
+        // AutoProps
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        // Constructors
+        public Position(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return $"{Linha}, {Coluna}";
+        }
+    }
+
+}
