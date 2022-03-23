438cbc638685be7d464bf33378a2bd5f6c0a61c3
Marcílio Júnior <marciliojr@outlook.com>
2022-03-23 11:09:40 -0300

    Melhorando a implementacao do metodo printBoard

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -8,14 +8,16 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            /*
+            
             try
             {
                 Board board = new Board(8, 8); // Cria um tabuleiro de 8x8 com todos os elementos nulos.
 
                 board.putPiece(new Rook(board, Color.Black), new Position(0, 0));
-                board.putPiece(new Rook(board, Color.Black), new Position(1, 9));
+                board.putPiece(new Rook(board, Color.Black), new Position(1, 7));
                 board.putPiece(new King(board, Color.Black), new Position(0, 2));
+                board.putPiece(new Rook(board, Color.White), new Position(3, 4));
+                board.putPiece(new King(board, Color.Black), new Position(2, 5));
 
                 Screen.PrintBoard(board);
             }
@@ -23,7 +25,8 @@ namespace Xadrez_Console
             {
                 Console.WriteLine(e.Message);
             }
-            */
+            
+            /*
             ChessPosition pos = new ChessPosition('a', 1);
             ChessPosition pos1 = new ChessPosition('c', 7);
             Console.WriteLine(pos);
@@ -31,7 +34,7 @@ namespace Xadrez_Console
             
             Console.WriteLine(pos1);
             Console.WriteLine(pos1.toPosition());
-
+            */
         }
     }
 }
--- Xadrez-Console/Screen.cs
+++ Xadrez-Console/Screen.cs
@@ -9,6 +9,7 @@ namespace Xadrez_Console
         {
             for (int i = 0; i < board.Rows; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Columns; j++)
                 {
                     if (board.piece(i, j) == null)
@@ -17,10 +18,26 @@ namespace Xadrez_Console
                     }
                     else
                     {
-                        Console.Write($"{board.piece(i, j)} ");
+                        printPiece(board.piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.Write("  a b c d e f g h");
+        }
+        public static void printPiece(Piece piece)
+        {
+            if (piece.color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
